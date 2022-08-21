e0390c6c8a446d7f6f7acab09a7cf2385a43cfbe
Martin Stojcevski <stojcevski.m95@gmail.com>
2022-08-21 23:47:27 +0200

    update
    update

--- Store/urls.py
+++ Store/urls.py
@@ -2,6 +2,9 @@ from django.urls import path,include
 from Store import views
 
 urlpatterns = [
+    path('hellomac/',views.hello_mac,name='hello_mac'),
     path('products/',views.ProductsListView.as_view(),name='product_list'),
     path('products/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
+    path('customp/', views.ProductsCustom.as_view(), name='custom_p'),
+    path('custompd/<int:product_id>', views.ProductCustomDetails.as_view(),name="custom_pd")
 ]
--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,5 @@
+from distutils.command.install_egg_info import safe_name
+from itertools import product
 from webbrowser import get
 from django.shortcuts import render, get_object_or_404
 from Store import serializers
@@ -6,13 +8,86 @@ from . models import *
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework import generics, mixins
-
 from rest_framework.views import APIView
 
 # Create your views here.
 
+@api_view(['GET','POST'])
+def hello_mac(request):
+    
+    if request.method == 'POST':
+        post_data = request.data
+        response = {
+            'message' : 'created',
+            'data': post_data
+        }
+
+        return Response(data=response)
+    
+    elif request.method == 'GET':    
+        return Response({'message': 'Hello Mac'})
+    
+
+class ProductsCustom(APIView):
+    
+    
+    
+    def get(self, request : Request):
+        
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+    
+        response = {
+            'message' : 'created',
+            'count' : queryset.count(),
+            'data': serializer.data
+ 
+        }
+        
+        return Response(data=response, status= status.HTTP_200_OK)
+    
+    
+    def post(self, request: Request):
+        
+        
+        serializer = ProductSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            response = {
+            'message' : 'product created!',
+            'data': serializer.data
+ 
+        }
+            return Response(data=response, status= status.HTTP_201_CREATED)
+
+class ProductCustomDetails(APIView):
+    
+    def put(self, request : Request, product_id):
+        
+        product = get_object_or_404(Product, pk=product_id)
+
+        serializer = ProductSerializer(product, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            
+            response = {
+            'message' : 'product created!',
+            'data': serializer.data
+ 
+        }      
+            return Response(data=serializer.data, status = status.HTTP_200_OK)
+    
+        return Response(data={'message': 'product not found!'}, status = status.HTTP_404_NOT_FOUND)
+    
+    def delete(self, request, product_id, format=None):
+        product = get_object_or_404(Product, pk=product_id)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 #Retreiving all products and creates a product      
 class ProductsListView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin
@@ -20,7 +95,6 @@ class ProductsListView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Cr
     
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
     
     def get(self, request: Request, *args, **kwargs):
             return self.list(request, *args, **kwargs)
