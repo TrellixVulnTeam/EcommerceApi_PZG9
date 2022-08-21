2be71deecfca02f95f145e5019aabce01b023499
Martin Stojcevski <stojcevski.m95@gmail.com>
2022-08-21 22:00:57 +0200

    changes
    changes

--- Store/urls.py
+++ Store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from Store import views
 
 urlpatterns = [
-    path('products/',views.ProductsList.as_view(),name='product_list'),
-    path('products/<int:pk>', views.ProductDetailsView.as_view(), name='product_details'),
+    path('products/',views.ProductsListView.as_view(),name='product_list'),
+    path('products/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
 ]
